6102ea036923ef9b2c213057d81cb54683a8bb88
yasminemasmoudi75 <yasminemasmoudi55@gmail.com>
2026-03-29 19:06:25 +0100

    back

--- diag_devis.js
+++
@@ -1,9 +0,0 @@
-const { sequelize } = require('./src/config/database');
-console.log('Sequelize:', typeof sequelize);
-try {
-    const Devis = require('./src/models/DevisMaster');
-    console.log('Devis loaded successfully');
-} catch (err) {
-    console.error('Error loading Devis:', err.message);
-    console.error(err.stack);
-}
